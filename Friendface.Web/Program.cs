f16b0f35a4f7a788de5ab88cde39c678a2bfc0b7
Teele Kaldaru <Teele.Kaldaru@uptime.eu>
2018-07-17 15:29:10 +0300

    added AI

--- Friendface.Web/Program.cs
+++ Friendface.Web/Program.cs
@@ -19,6 +19,7 @@ namespace Friendface.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
